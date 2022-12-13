e0b70c0615473166c40e9925a608c0efed3e7e6e
ulysseschaparro <77810864+ulysseschaparro@users.noreply.github.com>
2022-12-13 00:53:25 -0600

    Update i2c.v

--- i2c.v
+++ i2c.v
@@ -10,13 +10,6 @@
 
 // Target Platform: DE1-SoC Board
 
-// Hardware configuration:
-// GPIO Port:
-//   GPIO_1[31-0] is used as a general purpose GPIO port
-// HPS interface:
-//   Mapped to offset of 0 in light-weight MM interface aperature
-//   IRQ80 is used as the interrupt interface to the HPS
-
 //-----------------------------------------------------------------------------
 
 module i2c (clk, reset, address, byteenable, chipselect, writedata, 
@@ -34,27 +27,27 @@ module i2c (clk, reset, address, byteenable, chipselect, writedata,
     output reg [31:0]  readdata;
     
     // i2c bus/test signals conduit 
-	output reg 		 i2c_scl;
+    output reg 		 i2c_scl;
     inout reg		 i2c_sda;
-	output			 test_out;
-	
-	//debug conduit 
-	output 			 debug_read, debug_write, debug_chipselect; 		 
-	output [1:0] 	 debug_address; 
-	output [4:0]     debug_writedata; 
-	output [4:0]	 debug_readdata; 
-	output [4:0] 	 rd_index; 
-	output [4:0] 	 wr_index;
-	
-	assign debug_read = read;
-	assign debug_write = write; 
-	assign debug_chipselect = chipselect;
-	assign debug_address = address; 
-	assign debug_writedata[3:0] = d;
-	assign debug_writedata[4] = repeatbit;
-	assign debug_readdata = state [4:0];
-	assign rd_index = rd_index_tx [4:0]; 
-	assign wr_index = state [4:0]; //wr_index_tx [4:0];
+    output	    	 test_out;
+
+    //debug conduit 
+    output 	       debug_read, debug_write, debug_chipselect; 		 
+    output [1:0]       debug_address; 
+    output [4:0]       debug_writedata; 
+    output [4:0]       debug_readdata; 
+    output [4:0]       rd_index; 
+    output [4:0]       wr_index;
+
+    assign debug_read = read;
+    assign debug_write = write; 
+    assign debug_chipselect = chipselect;
+    assign debug_address = address; 
+    assign debug_writedata[3:0] = d;
+    assign debug_writedata[4] = repeatbit;
+    assign debug_readdata = state [4:0];
+    assign rd_index = rd_index_tx [4:0]; 
+    assign wr_index = state [4:0]; //wr_index_tx [4:0];
 
     // internal registers 
     reg [6:0]  address_reg;
@@ -62,7 +55,7 @@ module i2c (clk, reset, address, byteenable, chipselect, writedata,
     reg [31:0] status;			//actual status reg that you read from
     reg [31:0] control;
 	
-	reg [31:0] clear_status; 	//use this to write to status reg to clear
+    reg [31:0] clear_status; 	//use this to write to status reg to clear
     
     // register map 
     // ofs  fn
@@ -72,81 +65,81 @@ module i2c (clk, reset, address, byteenable, chipselect, writedata,
     //  12  control (r/w)
     
     // register numbers
-    parameter ADDRESS_REG         	= 2'b00;
+    parameter ADDRESS_REG           = 2'b00;
     parameter DATA_REG              = 2'b01;
     parameter STATUS_REG            = 2'b10;
     parameter CONTROL_REG   	    = 2'b11;
 
-	//edge detect for write & read
-	wire write_ed;
-	edge_detect wr(clk, write, write_ed);
-	wire read_ed;
-	edge_detect rd(clk, sm_rd_request, read_ed);
-	wire write_RX;
-	edge_detect RXwr(clk, sm_wr_requestRX, write_RX);
-	wire read_RX;
-	edge_detect RXrd(clk, read, read_RX);
-	
-	reg sm_rd_request; 
-	reg sm_wr_requestRX;
-	wire [7:0] sm_data;
-	
-	//tx fifo instantiation
-	wire [7:0] wr_data_tx = data;
-	wire [7:0] rd_data_tx;
-	wire [5:0] wr_index_tx;
-	wire [5:0] rd_index_tx;
-	wire empty_tx, full_tx, overflow_tx;
-	wire clear_overflow_tx = clear_status [3];
-	
-	fifo tx_fifo(
-	.clk(clk), 
-	.reset(reset), 
-	.clear_overflow_request(clear_overflow_tx),
-	.wr_data(wr_data_tx), 
-	.wr_request(write_ed & (address == DATA_REG)), 
-	.rd_request(read_ed),
-	.rd_data(sm_data), //?
-	.empty(empty_tx),
-	.full(full_tx),
-	.overflow(overflow_tx),
-	.wr_index(wr_index_tx),
-	.rd_index(rd_index_tx)); 			
+    //edge detect for write & read
+    wire write_ed;
+    edge_detect wr(clk, write, write_ed);
+    wire read_ed;
+    edge_detect rd(clk, sm_rd_request, read_ed);
+    wire write_RX;
+    edge_detect RXwr(clk, sm_wr_requestRX, write_RX);
+    wire read_RX;
+    edge_detect RXrd(clk, read, read_RX);
 
-	//rx fifo instantiation
-	reg [7:0] wr_data_rx; 
-	wire [7:0] rd_data_rx;  
-	wire [5:0] wr_index_rx;
-	wire [5:0] rd_index_rx; 
-	wire empty_rx, full_rx, overflow_rx;
-	wire clear_overflow_rx = clear_status [3];
-	
-	fifo rx_fifo(
-	.clk(clk), 
-	.reset(reset), 
-	.clear_overflow_request(clear_overflow_rx),
-	.wr_data(wr_data_rx), 
-	.wr_request(write_RX), 
-	.rd_request(0), //read_RX & (address == DATA_REG)),
-	.rd_data(rd_data_rx),
-	.empty(empty_rx),
-	.full(full_rx),
-	.overflow(overflow_rx),
-	.wr_index(wr_index_rx),
-	.rd_index(rd_index_rx)); 	
-	
-	always @ (posedge clk)
-	begin
-		status [0] <= overflow_rx; 
-		status [1] <= full_rx; 
-		status [2] <= empty_rx;
-		status [3] <= overflow_tx; 
-		status [4] <= full_tx;
-		status [5] <= empty_tx; 
-		status [13:8] <= wr_index_tx;		
-		status [19:14] <= rd_index_tx; 
-		status [31:20] <= 12'b000000000000;  
-	end
+    reg sm_rd_request; 
+    reg sm_wr_requestRX;
+    wire [7:0] sm_data;
+
+    //tx fifo instantiation
+    wire [7:0] wr_data_tx = data;
+    wire [7:0] rd_data_tx;
+    wire [5:0] wr_index_tx;
+    wire [5:0] rd_index_tx;
+    wire empty_tx, full_tx, overflow_tx;
+    wire clear_overflow_tx = clear_status [3];
+
+    fifo tx_fifo(
+    .clk(clk), 
+    .reset(reset), 
+    .clear_overflow_request(clear_overflow_tx),
+    .wr_data(wr_data_tx), 
+    .wr_request(write_ed & (address == DATA_REG)), 
+    .rd_request(read_ed),
+    .rd_data(sm_data), //?
+    .empty(empty_tx),
+    .full(full_tx),
+    .overflow(overflow_tx),
+    .wr_index(wr_index_tx),
+    .rd_index(rd_index_tx)); 			
+
+    //rx fifo instantiation
+    reg [7:0] wr_data_rx; 
+    wire [7:0] rd_data_rx;  
+    wire [5:0] wr_index_rx;
+    wire [5:0] rd_index_rx; 
+    wire empty_rx, full_rx, overflow_rx;
+    wire clear_overflow_rx = clear_status [3];
+
+    fifo rx_fifo(
+    .clk(clk), 
+    .reset(reset), 
+    .clear_overflow_request(clear_overflow_rx),
+    .wr_data(wr_data_rx), 
+    .wr_request(write_RX), 
+    .rd_request(0), //read_RX & (address == DATA_REG)),
+    .rd_data(rd_data_rx),
+    .empty(empty_rx),
+    .full(full_rx),
+    .overflow(overflow_rx),
+    .wr_index(wr_index_rx),
+    .rd_index(rd_index_rx)); 	
+
+    always @ (posedge clk)
+    begin
+	status [0] <= overflow_rx; 
+	status [1] <= full_rx; 
+	status [2] <= empty_rx;
+	status [3] <= overflow_tx; 
+	status [4] <= full_tx;
+	status [5] <= empty_tx; 
+	status [13:8] <= wr_index_tx;		
+	status [19:14] <= rd_index_tx; 
+	status [31:20] <= 12'b000000000000;  
+    end
         
     // read register
     always @ (*)
@@ -156,7 +149,7 @@ module i2c (clk, reset, address, byteenable, chipselect, writedata,
                 ADDRESS_REG: 
                     readdata = address_reg;
                 DATA_REG:
-							readdata = rd_data_rx;
+		    readdata = rd_data_rx;
                 STATUS_REG: 
                     readdata = status;
                 CONTROL_REG: 
@@ -169,8 +162,8 @@ module i2c (clk, reset, address, byteenable, chipselect, writedata,
     // write register
     always @ (posedge clk)
     begin
-		if (busy)
-			control [17] <= 1'b0; 
+	if (busy)
+	    control [17] <= 1'b0; 
         if (reset)
         begin
             address_reg [6:0] <= 7'b0;
@@ -186,55 +179,55 @@ module i2c (clk, reset, address, byteenable, chipselect, writedata,
                     ADDRESS_REG: 
                         address_reg <= writedata;
                     DATA_REG: 
-						data <= writedata;
+		        data <= writedata;
                     STATUS_REG: 
                         clear_status <= writedata;
                     CONTROL_REG: 
                         control <= writedata;
                 endcase
             end
-			else
-				clear_status <= 32'b0; 
+	    else
+	        clear_status <= 32'b0; 
         end 
     end
 	
     //200 kHz internal reference for state change
-	reg [7:0] count; 
-	always @ (posedge(clk))
+    reg [7:0] count; 
+    always @ (posedge(clk))
+    begin
+	direction_reg <= control [0];
+			
+	if (count == 250) //50,000,000 / 200,000
 	begin
-		direction_reg <= control [0];
+		state <= next_state;
+		count <= 0;
 			
-		if (count == 250) //50,000,000 / 200,000
-		begin
-			state <= next_state;
-			count <= 0;
-			
-			if (state == START2_STATE)
-				n <= 6; 
+		if (state == START2_STATE)
+			n <= 6; 
 				
-			if (state == ADDRESS_WRITE2_STATE)
-				n <= n - 1;
+		if (state == ADDRESS_WRITE2_STATE)
+			n <= n - 1;
 				
-			if (state == ACK_ADDRESS2_STATE)
-				r <= 7;
+		if (state == ACK_ADDRESS2_STATE)
+			r <= 7;
 				
-			if (state == REG_WRITE2_STATE)
-				r <= r - 1;
+                if (state == REG_WRITE2_STATE)
+                        r <= r - 1;
 				
-			if (state == ACK_REG2_STATE)
-				d <= 7; 
+                if (state == ACK_REG2_STATE)
+                        d <= 7; 
 				
-			if (state == DATA_READ2_STATE)
-				wr_data_rx[d2] <= i2c_sda;
+                if (state == DATA_READ2_STATE)
+                        wr_data_rx[d2] <= i2c_sda;
 
-			if (state == DATA_WRITE2_STATE)
-				d <= d - 1;
+                if (state == DATA_WRITE2_STATE)
+                        d <= d - 1;
 
-			if(state == DATA_READ1_STATE)
-				d2 <= d2 - 1;
-		end
-		else
-			count <= count + 1; 
+                if(state == DATA_READ1_STATE)
+                        d2 <= d2 - 1;
+	end
+	else
+		count <= count + 1; 
 	end
 	
 	parameter IDLE_STATE            = 0;
@@ -242,23 +235,23 @@ module i2c (clk, reset, address, byteenable, chipselect, writedata,
 	parameter START2_STATE         	= 2;
 	parameter ADDRESS_WRITE1_STATE 	= 3;
 	parameter ADDRESS_WRITE2_STATE 	= 4;
-	parameter RW1_STATE				= 5; 
-	parameter RW2_STATE				= 6; 
+	parameter RW1_STATE		= 5; 
+	parameter RW2_STATE		= 6; 
 	parameter ACK_ADDRESS1_STATE	= 7;
 	parameter ACK_ADDRESS2_STATE	= 8;
 	parameter REG_WRITE1_STATE      = 9;
-	parameter REG_WRITE2_STATE 		= 10; 
-	parameter ACK_REG1_STATE		= 11;
-	parameter ACK_REG2_STATE		= 12;
-	parameter DATA_WRITE1_STATE		= 13; 
-	parameter DATA_WRITE2_STATE		= 14; 
-	parameter DATA_READ1_STATE 		= 15; 
-	parameter DATA_READ2_STATE 		= 16; 
-	parameter ACK_DATA1_STATE		= 17;
-	parameter ACK_DATA2_STATE		= 18;	
+	parameter REG_WRITE2_STATE 	= 10; 
+	parameter ACK_REG1_STATE	= 11;
+	parameter ACK_REG2_STATE	= 12;
+	parameter DATA_WRITE1_STATE	= 13; 
+	parameter DATA_WRITE2_STATE	= 14; 
+	parameter DATA_READ1_STATE 	= 15; 
+	parameter DATA_READ2_STATE 	= 16; 
+	parameter ACK_DATA1_STATE	= 17;
+	parameter ACK_DATA2_STATE	= 18;	
 	parameter PRESTOP_STATE         = 19;
 	parameter STOP1_STATE           = 20;
-	parameter STOP2_STATE			= 21;
+	parameter STOP2_STATE		= 21;
 	parameter REPEATED_START1_STATE	= 22;
 	parameter REPEATED_START2_STATE	= 23;
 	
